fa3842595d58dd2dc81d2bd0d47a4d0ec18601ef
chiru1221 <takuro1221@DESKTOP-2KGDHMJ.localdomain>
2020-08-18 01:46:57 +0900

    src

--- simple_bnn.py
+++ simple_bnn.py
@@ -8,3 +8,9 @@ tf.keras.backend.set_floatx('float64')
 gpus= tf.config.experimental.list_physical_devices('GPU')
 if len(gpus) != 0:
     tf.config.experimental.set_memory_growth(gpus[0], True)
+
+class BNN(tf.keras.Model):
+    def __init__(self, vocab_size, embed_dim):
+        super().__init__()
+        self.embed = tf.keras.layers.Embedding(vocab_size, embed_dim)
+        self.fc1 = tfp.layers.DenseFlipout
